f8f86200e113258cd613847263e4f1f0b493bdf6
dathdka <dathdka@gmail.com>
2023-01-30 21:36:17 +0700

    adding mysql

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,29 @@ import cors from "cors"
 import  express  from "express"
 import http from "http"
 import mongoose from "mongoose"
+import mysql from "mysql"
 import 'dotenv/config'
 
 var app = express();
 app.use(express.json())
 app.use(cors({origin: true}))
 const server = http.createServer(app);
-mongoose.set('strictQuery',true)
-mongoose.connect(`${process.env.MONGO_LOCAL}`).then(()=>{
-    server.listen(1245, () =>{
-        console.log('server listening port 1245')
-    })
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'crawl'
 })
+
+connection.connect(() : void=>{
+    server.listen(1245, () : void =>{
+        connection.query(`create table test ( 
+            id      int
+            name    string 
+            )`)
+        console.log('server listening port 1246')
+        return ;
+    })
+});
+// mongoose.set('strictQuery',true)
+// mongoose.connect(`${process.env.MONGO_LOCAL}`).then(()=>{
+// })
