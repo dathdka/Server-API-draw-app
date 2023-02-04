48d7e3827634a63fbdb358fb73d71eee784d385a
dathdka <dathdka@gmail.com>
2023-02-04 22:58:22 +0700

    adding redis

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,30 @@
-import cors from "cors"
-import  express  from "express"
-import http from "http"
-import dbconfig from './dbConfig/config'
-import router from './routes/router'
-import redis from 'redis'
-import 'dotenv/config'
-
+import cors from "cors";
+import express from "express";
+import http from "http";
+import dbconfig from "./dbConfig/config";
+import router from "./routes/router";
+import { redisClient } from "./initRedis";
+import "dotenv/config";
 
 var app = express();
-app.use(express.json())
-app.use(cors({origin: true}))
-app.use('/home', router)
+
+
+app.use(express.json());
+app.use(cors({ origin: true }));
+app.use("/home", router);
 const server = http.createServer(app);
-  
-dbconfig.sync().then(()=>{
-    console.log('DB synced successfully') 
-    server.listen(1245, () : void =>{ 
-        console.log('server listening port 1245') 
-    })
-}).catch((err : Error) =>{
-    throw err 
-})
+(async() => await new redisClient().getClient())()
+
+dbconfig
+  .sync()
+  .then(() => {
+    console.log("DB synced successfully");
+    server.listen(`${process.env.EXPRESS_PORT}`, (): void => {
+      console.log(`server listening port ${process.env.EXPRESS_PORT}`);
+    });
+  })
+  .catch((err: Error) => {
+    throw err;
+  });
+
+
---
+++ src/redis.ts
@@ -0,0 +1,30 @@
+import * as redis from "redis";
+import "dotenv/config";
+
+export class redisClient {
+  static client: redis.RedisClientType;
+
+  constructor() {}
+
+  public async getClient() {
+    if (!redisClient.client) {
+      redisClient.client = redis.createClient();
+      redisClient.client.on("error", (err) => {
+        console.error(err);
+      });
+      redisClient.client.on("connect", () => {
+        console.log(`redis client has been started`);
+      });
+      await redisClient.client.connect();
+    }
+    return new redisClient();
+  }
+
+  public async setValue(key: string, value: string) {
+    await redisClient.client.set(key, value);
+  }
+
+  public async getValue(key: string) {
+    return await redisClient.client.get(key);
+  }
+}
